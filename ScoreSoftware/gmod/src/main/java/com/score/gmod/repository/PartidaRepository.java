790b50974108199b7cb31d63037e1ff7608a0ba0
LUCIANO DE LIMA MOURA <roshinomo@gmail.com>
2026-04-07 11:49:41 -0400

    Delete ScoreSoftware/gmod directory

--- ScoreSoftware/gmod/src/main/java/com/score/gmod/repository/PartidaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.score.gmod.repository;
-
-import com.score.gmod.model.Partida;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartidaRepository extends JpaRepository<Partida, Long> {
-}
