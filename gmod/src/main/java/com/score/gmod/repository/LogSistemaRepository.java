3f4c3aea1e9cb1196551d4109fb9ef40bd81235d
LUCIANO DE LIMA MOURA <roshinomo@gmail.com>
2026-04-07 11:49:57 -0400

    Delete gmod directory

--- gmod/src/main/java/com/score/gmod/repository/LogSistemaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.score.gmod.repository;
-
-import com.score.gmod.model.LogSistema;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogSistemaRepository extends JpaRepository<LogSistema, Long> {
-}
