df322b66055b7448a2917e54fa3151b865d83679
noronham464-design <noronham464@gmail.com>
2026-04-08 09:46:13 -0300

    Change Conexao class to package-private

--- Model Usuario/config_conexão.java
+++ Model Usuario/config_conexão.java
@@ -3,9 +3,9 @@ package com.score.garrys.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class Conexao {
+class Conexao {
 
-    public static Connection getConnection() throws Exception {
+    static Connection getConnection() throws Exception {
         return DriverManager.getConnection(
             "jdbc:mysql://localhost:3306/seubanco",
             "root",
