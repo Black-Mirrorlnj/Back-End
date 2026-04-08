37344592d60b0835af2e4aa2a3cc9463a654e146
noronham464-design <noronham464@gmail.com>
2026-04-08 12:30:02 -0300

    Update Atualizar usuario.java

--- garrys/src/main/java/com/score/garrys/Usuario/controller/Atualizar usuario.java
+++ garrys/src/main/java/com/score/garrys/Usuario/controller/Atualizar usuario.java
@@ -1,1 +1,5 @@
-
+    @PutMapping("/{id}")
+    Usuario atualizarUsuario(@PathVariable int id, @RequestBody Usuario usuario) {
+        usuario.setUserId(id);
+        return usuarioRepository.update(usuario);
+    }
