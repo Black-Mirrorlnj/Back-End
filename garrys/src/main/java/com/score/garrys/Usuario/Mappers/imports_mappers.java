da8c8778938aa2bdc4358583a9934ff43c4c758c
noronham464-design <noronham464@gmail.com>
2026-04-09 16:22:35 -0400

    Create imports_mappers.java for Usuario mapping

---
+++ garrys/src/main/java/com/score/garrys/Usuario/Mappers/imports_mappers.java
@@ -0,0 +1,5 @@
+package com.score.garrys.mapper;
+import com.score.garrys.model.Usuario;
+import com.score.garrys.dto.UsuarioRequest;
+import com.score.garrys.dto.UsuarioResponse;
+class UsuarioMapper {
