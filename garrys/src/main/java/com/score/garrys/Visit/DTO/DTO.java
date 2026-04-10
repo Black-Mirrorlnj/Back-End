e06a956b3eea604569bfe226984cea94d963225d
JulioGB <juliogbp2000@gmail.com>
2026-04-10 08:44:11 -0400

    Create DTO.java

---
+++ garrys/src/main/java/com/score/garrys/Visit/DTO/DTO.java
@@ -0,0 +1,39 @@
+package com.score.gmod.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public class VisitanteDTO {
+
+    private int idVisitante;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @Min(value = 0, message = "Kills não pode ser negativo")
+    private int kills;
+
+    public int getIdVisitante() {
+        return idVisitante;
+    }
+
+    public void setIdVisitante(int idVisitante) {
+        this.idVisitante = idVisitante;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getKills() {
+        return kills;
+    }
+
+    public void setKills(int kills) {
+        this.kills = kills;
+    }
+}
